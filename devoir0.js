7ae9a99b19ed06102cd3a4514f6850b66d8eca2a
Ricky <dengruiqi1028@qq.com>
2019-01-13 00:42:50 -0500

    Update devoir0.js

--- devoir0.js
+++ devoir0.js
@@ -1,1 +1,10 @@
-var 
+var prenom="Ruiqi";
+var nom="Deng";
+var salutation="Bonjour, mon nom est " + prenom +" "+ nom +"!";
+var age=32;
+var discipline="Informatique";
+var informations="J'ai " + age +" ans et j'étudie en " + discipline +"!";
+var introduction=salutation+"\n"+informations;
+print (introduction);
+
+load ("correction.js");
